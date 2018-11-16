d9e0f2bbc943bf8c638cc8ad4dd2c78141469bec
FourDesigners <44194063+FourDesigners@users.noreply.github.com>
2018-11-16 10:54:03 +0100

    Added "findMissingRecords()" method; backup files added in /Documenti, edited files in /Files (now suitable for tests)

--- 4Designers_ITSS/src/etlScript/Main.java
+++ 4Designers_ITSS/src/etlScript/Main.java
@@ -11,11 +11,14 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.TreeSet;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -29,6 +32,7 @@ public class Main {
     private static final String PATH_SOURCE_FILE = "Files/incidenti.csv";
     private static final String PATH_TEMP_FILE = "Files/temp.csv";
     private static final String PATH_DEST_FILE = "Files/new_incidenti.csv";
+    private static final String PATH_MISSINGS_FILE = "Files/mancanti.txt";
     private static final String SEPARATOR = ";";
     private static final int N_COLONNE = 55;
     
@@ -61,7 +65,6 @@ public class Main {
                     if(record.isCorrect()) {
                         if (!record.isPresent(chiavi)) { //controllo duplicati
                             chiavi.add(new Chiave(campi[0], campi[2]));
-                            //TODO: Verificare righe mancanti
                             out.print(campi[0]);
                             for(int i=1; i<campi.length; i++) {
                                 out.print(SEPARATOR + campi[i]);
@@ -81,7 +84,8 @@ public class Main {
             }
             out.close();
             inputStream.close();
-            update(PATH_TEMP_FILE, PATH_DEST_FILE); //aggiunge le nuove colonne
+            findMissingRecords(PATH_DEST_FILE, PATH_TEMP_FILE, comuni); //controlla se ci sono righe mancanti
+            update(PATH_TEMP_FILE, PATH_DEST_FILE); //aggiunge le nuove righe al file dest
             System.out.println("Numero righe duplicate : " + duplicati);
         }catch(FileNotFoundException e) {
             System.out.println("Non è stato trovato il file");
@@ -134,5 +138,81 @@ public class Main {
         } catch (FileNotFoundException ex) {
             System.out.println("File dei comuni non presente");
         }
+    }
+    
+    private static int findMissingRecords(String finalFile, String tempFile, Map<String, Set<String>> provinceComuni) {
+        Scanner inputStream;
+        String riga;
+        String[] campi;
+        PrintWriter out;
+        
+        try {
+            //creo la lista degli anni
+            TreeSet<Integer> anniPresenti = new TreeSet<Integer>();
+            //popolo la lista degli anni con quelli presenti nel file Temp
+            inputStream = new Scanner(new File(tempFile));
+            while(inputStream.hasNextLine()) {
+                riga = inputStream.nextLine();
+                campi = riga.split(SEPARATOR);
+                anniPresenti.add(Integer.parseInt(campi[0]));
+            }
+            inputStream.close();
+            //popolo la lista degli anni con quelli presenti nel file Dest
+            inputStream = new Scanner(new File(finalFile));
+            riga = inputStream.nextLine(); //salto la prima riga
+            while(inputStream.hasNextLine()) {
+                riga = inputStream.nextLine();
+                campi = riga.split(SEPARATOR);
+                anniPresenti.add(Integer.parseInt(campi[0]));
+            }
+            inputStream.close();
+            Iterator<Integer> anniItr = anniPresenti.iterator();
+            //creo lista di comuni senza le province, popolandola tramite l'HashMap creato nel main
+            ArrayList<String> comuni = new ArrayList<String>();
+            for(String provincia : provinceComuni.keySet()) {
+                comuni.addAll(provinceComuni.get(provincia));
+            }
+            while(anniItr.hasNext()) {
+                //prendo l'anno che sto analizzando
+                int annoAnalizzato = anniItr.next();
+                ArrayList<String> comuniCopia = new ArrayList<String>(comuni);
+                //elimino da comuniCopia i comuni che ho trovato nel file temp
+                inputStream = new Scanner(new File(tempFile));
+                while(inputStream.hasNextLine()) {
+                    riga = inputStream.nextLine();
+                    campi = riga.split(SEPARATOR);
+                    if(comuniCopia.contains(campi[2]))
+                        comuniCopia.remove(campi[2]);
+                }
+                inputStream.close();
+                //elimino da comuniCopia i comuni che ho trovato nel file final
+                inputStream = new Scanner(new File(finalFile));
+                riga = inputStream.nextLine(); //salto la prima riga
+                while(inputStream.hasNextLine()) {
+                    riga = inputStream.nextLine();
+                    campi = riga.split(SEPARATOR);
+                    if(comuniCopia.contains(campi[2]))
+                        comuniCopia.remove(campi[2]);
+                }
+                inputStream.close();
+                //System.out.println("Per l'anno " + annoAnalizzato + " mancano i seguenti comuni: " + comuniCopia);
+                if(!new File(PATH_DEST_FILE).exists()) 
+                    out = new PrintWriter(new BufferedWriter(new FileWriter(PATH_MISSINGS_FILE)));
+                else out = new PrintWriter(new BufferedWriter(new FileWriter(PATH_MISSINGS_FILE, true)));
+                if(comuniCopia.isEmpty())
+                    out.println("Sono stati inseriti tutti i dati per ogni comune.");
+                else {
+                    out.println("Per l'anno " + annoAnalizzato + " mancano i seguenti comuni: " + comuniCopia);
+                    out.println("\n");
+                    out.close();
+                }
+            }
+        } catch (FileNotFoundException e1) {
+            System.out.println("Uno dei file non è stato trovato");
+        } catch (IOException e2) {
+            System.out.println("Impossibile aprire il file per scrivere i record mancanti");
+        }
+        
+        return 0;
     }
 }
